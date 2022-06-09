84ee4964a693b698f7cee4431b58fdbfb5a59499
Y. Shen <P302656@workspace.rug.nl>
2022-06-09 16:05:05 +0200

    0609

--- R/plot_figures.R
+++ R/plot_figures.R
@@ -18,6 +18,20 @@
   # par(mfrow = c(1, 1))
   #
 
-
+# sim_out <- list(out_1, out_2, out_3, out_4)
+# species_mean <-  matrix(nrow = 4, ncol = 6)
+# colnames(species_mean) <- c("nIp", "nAp", "nCp", "nIa", "nAa", "nCa")
+# for (j in 1:4){
+#   for (i in 1:20){
+#     stt_table <- sim_out[[j]][[i]][["island"]][["stt_table"]]
+#     nIp <- floor(mean(c(nIp, stt_table[nrow(stt_table), 2])))
+#     nAp <- floor(mean(c(nAp, stt_table[nrow(stt_table), 3])))
+#     nCp <- floor(mean(c(nCp, stt_table[nrow(stt_table), 4])))
+#     nIa <- floor(mean(c(nIa, stt_table[nrow(stt_table), 5])))
+#     nAa <- floor(mean(c(nAa, stt_table[nrow(stt_table), 6])))
+#     nCa <- floor(mean(c(nCa, stt_table[nrow(stt_table), 7])))
+#   }
+#   species_mean[j, ] <- c(nIp, nAp, nCp, nIa, nAa, nCa)
+# }
 
 
