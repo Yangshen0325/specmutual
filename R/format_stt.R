0e657959b47dd84834b061badb067f8995d6ccd5
Y. Shen <P302656@workspace.rug.nl>
2022-06-14 16:07:25 +0200

    changed stt_final to stt_all

--- R/format_stt.R
+++ R/format_stt.R
@@ -1,4 +1,4 @@
-# load()
+# load("X:/YSPhD_Aca/specmutual/result/out_5.RData")
 # islands <- out_5
 
 
@@ -9,21 +9,21 @@ format_stt <- function(islands,
   for (i in 1:length(islands)){
     the_island <- islands[[i]]
     the_stt <- the_island[["island"]][["stt_table"]]
-    stt_final <- matrix(ncol = 7, nrow = sample_freq + 1)
-    colnames(stt_final) <- c("Time", "nIp", "nAp", "nCp", "nIa", "nAa", "nCa")
-    stt_final[, "Time"] <- rev(seq(from = 0,
-                                   to = total_time,
+    stt_all <- matrix(ncol = 7, nrow = sample_freq + 1)
+    colnames(stt_all) <- c("Time", "nIp", "nAp", "nCp", "nIa", "nAa", "nCa")
+    stt_all[, "Time"] <- rev(seq(from = 0,
+                                   to = simtime,
                                    length.out = sample_freq + 1))
-    stt_final[1, 2:7] <- c(0, 0, 0, 0, 0, 0)
+    stt_all[1, 2:7] <- c(0, 0, 0, 0, 0, 0)
 
-    for (j in 2:nrow(stt_final)) {
-      the_age <- stt_final[j, "Time"]
-      stt_final[j, 2:7] <- the_stt[max(which(the_stt[, "Time"] >= the_age)), 2:7]
+    for (j in 2:nrow(stt_all)) {
+      the_age <- stt_all[j, "Time"]
+      stt_all[j, 2:7] <- the_stt[max(which(the_stt[, "Time"] >= the_age)), 2:7]
     }
 
     island_list <- list() # I didn't write `not_present`
     island_list[[1]] <- list(island_age = simtime,
-                             stt_final = stt_final)
+                             stt_all = stt_all)
 
     taxon_list_size <- length(the_island[["island"]][["taxon_list"]])
       if (taxon_list_size != 0) {
