7239ca4ed97b9156029b2dc89f2af86d5a43e2f3
Yangshen0325 <sylviadyx77@163.com>
2022-09-28 16:03:18 +0200

    0928

--- script/job_simu_two.R
+++ script/job_simu_two.R
@@ -1,6 +1,6 @@
 
 M0 <- {set.seed(122); specmutual::get_M0(plant = 40)}
-mutualism_pars <- create_mutualism_pars(
+mutualism_pars <- specmutual::create_mutualism_pars(
   lac_pars = c(0.5, 0.5), # c(lac_plant, lac_animal)
   mu_pars = c(0.2, 0.2, 2.0, 2.0), # c(mu_P0, mu_A0, mu_P1, mu_A1)
   K_pars = c(Inf, Inf, 2.0, 2.0), # c(K_P0, K_A0, K_P1, K_A1)
