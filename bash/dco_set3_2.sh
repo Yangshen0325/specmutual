5c8fa122ae4561fa7839a82c64a5840721531403
Y. Shen <P302656@workspace.rug.nl>
2022-12-06 15:05:58 +0100

    1206

--- bash/dco_set3_2.sh
+++ bash/dco_set3_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=d_co_set3
-#SBATCH --output=logs/d_co_set3-%j.log
+#SBATCH --job-name=dco_set3_2
+#SBATCH --output=logs/dco_set3_2-%j.log
 #SBATCH --mem=4GB
 #SBATCH --partition=regular
 
@@ -11,4 +11,4 @@ ml R/4.2.0-foss-2021b
 
 Rscript -e "devtools::install_github('Yangshen0325/specmutual')"
 
-Rscript ~/specmutual/script/job_differ_coefficient_set3.R
+Rscript ~/specmutual/script/job_dcoset3_2.R
