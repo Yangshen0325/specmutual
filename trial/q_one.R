51742e40547735c66de97f576fc07390841a1261
Y. Shen <P302656@workspace.rug.nl>
2023-03-09 17:16:43 +0100

    stuff related to question one

--- trial/q_one.R
+++ trial/q_one.R
@@ -3,11 +3,11 @@ M0 <- matrix(1, ncol = 10, nrow = 10)
 M0[lower.tri(M0)] <-0
 M0<-M0[,c(10:1)]
 
-set2 <- create_mutualism_pars(
+set <- create_mutualism_pars(
   lac_pars = c(0.0, 0.0),
   mu_pars = c(0.0, 0.0, 0.0, 0.0),
-  K_pars = c(20, 20, 3.0, 3.0),
-  gam_pars = c(0.05, 0.05),
+  K_pars = c(100, 100, 1.0, 1.0),
+  gam_pars = c(0.5, 0.5),
   laa_pars = c(0.0, 0.0, 0.0, 0.0),
   qgain = 0.0,
   qloss = 0.0,
@@ -20,19 +20,38 @@ out_list <- list()
 for (loop in 1:1000){
   print(loop)
   set.seed(loop)
-  r2 <- sim_core_mutualism(total_time = 5, mutualism_pars = set2)
-  island_spec <- r2[["island_spec"]]
+  island_spec <- trial_sim(total_time = 5, mutualism_pars = set)
+
   #if (is.null(island_spec)) stop("here it is")
   out_list[[length(out_list) + 1]] <- island_spec
 }
-save(out_list, file = "D:/YSPhD_Aca/specmutual/trial/out_list.RData")
+# save(out_list, file = "D:/YSPhD_Aca/specmutual/trial/out_list.RData")
 ## deal with data
 library(tidyverse)
 data <- map2_dfr(out_list, seq(out_list), ~ .x %>%
                    as_tibble %>%
                    mutate(sim_id = .y))
 
-data %>%
+freq_out <- data %>%
   group_by(`Species state`, Species) %>%
   summarize(n = n()) %>%
   View
+# freq_out_p <- freq_out[1:10, ]
+# freq_out_a <- freq_out[11:20, ]
+#
+# ggplot(freq_out_p,
+#        aes(x = Species, y= n)) +
+#   geom_bar(stat = 'identity')
+#
+# ggplot(freq_out_a,
+#        aes(x = Species, y= n)) +
+#   geom_bar(stat = 'identity')
+freq_out$Species <- factor(freq_out$Species,
+                           levels = as.character(c(1:10)))
+ggplot(freq_out,
+       aes(x=`Species state`,y=n,fill=Species)) +
+  geom_col(position = 'dodge')
+
+ggsave("D:/YSPhD_Aca/specmutual/trial/out_list.tiff",
+       width = 4, height = 4, dpi = 300)
+dev.off()
