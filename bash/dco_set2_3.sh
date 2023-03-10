8b0071045e666b3ca5a88040d5f9bbad346c227e
Y. Shen <P302656@workspace.rug.nl>
2023-03-10 16:30:45 +0100

    1003_23

---
+++ bash/dco_set2_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=dco_set2_3
+#SBATCH --output=logs/dco_set2_3-%j.log
+#SBATCH --mem=4GB
+#SBATCH --partition=regular
+
+ml R/4.2.0-foss-2021b
+
+Rscript -e "devtools::install_github('Yangshen0325/specmutual')"
+
+Rscript ~/specmutual/script/job_dcoset2_3.R
