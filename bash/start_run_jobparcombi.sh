a97adf0a72fb3c1a087c527b1ae42168b718d484
Yangshen0325 <sylviadyx77@163.com>
2023-04-18 13:42:55 +0200

    1804_23

---
+++ bash/start_run_jobparcombi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=start_run_jobparcombi
+#SBATCH --output=logs/start_run_jobparcombi.log
+#SBATCH --mem=1GB
+#SBATCH --partition=short
+
+ml R
+
+Rscript -e "devtools::install_github('Yangshen0325/specmutual')"
+
+for (( param_set = 1; param_set <= 5; param_set++ ))
+do
+sbatch ~/specmutual/bash/run_job_par_combi.sh ${param_set}
+done
