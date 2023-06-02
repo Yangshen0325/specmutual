d3b9d5b9d8fc7fe35cbb46bee3e1a014ba8c49b8
Yangshen0325 <sylviadyx77@163.com>
2023-06-02 10:21:25 +0200

    add more to compute metrics

--- trial/plots.R
+++ trial/plots.R
@@ -27,6 +27,61 @@ boxplot(data$plant, data$animal, data$total,
         ylab="Count", ylim=c(0, 300))
 #################
 
+#### Compute modularity, nestedness and connectance ####
+library(bipartite)
+library(igraph)
 
+my_path <- "D:/PhD_Yang/literature/weboflife/web-of-life_MPL_allnet"
+file_list <- list.files(path = my_path, pattern = "\\.csv", full.names = TRUE)
+
+compute_network_metrics <- function(network) {
+  binary_network <- ifelse(network > 0, 1, 0) # those networks are the
+                                 #number of visiting observation
+
+  modularity <- DIRT_LPA_wb_plus(binary_network)$likelihood
+  nestedness <- nested(binary_network)
+  ## connectance
+  possible_links <- nrow(binary_network) * ncol(binary_network)
+  real_links <- sum(binary_network)
+  connectance <- possible_links/real_links
+  # Some of them can be NULL
+  if (is.null(modularity)) {
+    metrics <- data.frame(Modularity = NA, Nestedness = nestedness, Connectance = connectance)
+  } else if (is.null(nestedness)) {
+    metrics <- data.frame(Modularity = modularity, Nestedness = NA, Connectance = connectance)
+  } else if (is.null(connectance)) {
+    metrics <- data.frame(Modularity = modularity, Nestedness = nestedness, Connectance = NA)
+  } else {
+    metrics <- data.frame(Modularity = modularity, Nestedness = nestedness, Connectance = connectance)
+  }
+
+  return(metrics)
+}
+
+results <- list()
+for (file in file_list) {
+  print(file)
+  network <- read.csv(file, header = TRUE)
+  metrics <- compute_network_metrics(network)
+  results[[file]] <- metrics
+}
+
+#### plot data from island and mainland ####
+library(ggplot2)
+library(rnaturalearth) # can have a world map
+library(readxl) # or save as .csv (I wanna see whole data)
+
+data <- read_excel("D:/Desktop/network_info.xlsx")
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot() +
+  geom_sf(data = world, fill = "gray80")+
+  geom_point(data = data, aes(x = Longitude, y = Latitude, color = Type, shape = Type), size = 3) +
+  scale_fill_manual(values = c("mainland" = "blue", "island" = "red")) +
+  labs(title = "Observations",
+       x = "Longitude",
+       y = "Latitude",
+       color = "Type") +
+  theme_minimal()
 
 
