4c5f1cc4d22986ecedc0cfc1fa905bafbcf1e16f
Yangshen0325 <sylviadyx77@163.com>
2023-06-02 15:00:31 +0200

    0206

--- bash/run_par1.sh
+++ bash/run_par1.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=job_par_combi
-#SBATCH --output=logs/job_par_combi-%j.log
+#SBATCH --job-name=job_par1
+#SBATCH --output=logs/0206-%j.log
 #SBATCH --mem=4GB
 #SBATCH --partition=regular
 
