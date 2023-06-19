155c92e1c9e025801dcfc5c38f22baa80af1dc54
Yangshen0325 <sylviadyx77@163.com>
2023-06-19 15:42:35 +0200

    1906_23

---
+++ bash/run_par_combo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=job_par_comb0
+#SBATCH --output=logs/5-%j.log
+#SBATCH --mem=4GB
+#SBATCH --partition=regular
+#SBATCH --array=1-4
+
+ml R
+
+Rscript -e "devtools::install_github('Yangshen0325/specmutual')"
+
+param_set=$SLURM_TASK_ARRAY_ID
+Rscript ~/specmutual/script/job_par_combo.R ${param_set}
