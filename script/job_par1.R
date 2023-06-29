4825646bb49d1f53933c176b5de095ac4fa99538
Y. Shen <P302656@workspace.rug.nl>
2023-06-29 11:37:44 +0200

    change parameters

--- script/job_par1.R
+++ script/job_par1.R
@@ -3,11 +3,11 @@ load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
   lac_pars = c(1.0, 1.0),
   mu_pars = c(0.1, 0.1, 0.001, 0.001),
-  K_pars = c(50, 50, 0.5, 0.5),
+  K_pars = c(Inf, Inf, 0.5, 0.5),
   gam_pars = c(0.05, 0.05),
   laa_pars = c(1.0, 1.0, 0.1, 0.1),
-  qgain = 0.01,
-  qloss = 0.01,
+  qgain = 0.005,
+  qloss = 0.005,
   lambda0 = 0.5,
   M0 = M0,
   transprob = 1.0
