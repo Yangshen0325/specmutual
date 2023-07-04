373d2e7bbd92295c1eb27f4d91184d988af1b789
Y. Shen <P302656@workspace.rug.nl>
2023-07-04 15:14:27 +0200

    The previous one was out of memory so I lower down speciation rates

--- script/job_par1.R
+++ script/job_par1.R
@@ -1,11 +1,11 @@
 library(specmutual)
 load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
-  lac_pars = c(1.0, 1.0),
+  lac_pars = c(0.4, 0.4),
   mu_pars = c(0.2, 0.2, 0, 0),
   K_pars = c(Inf, Inf, 0, 0),
   gam_pars = c(0.05, 0.05),
-  laa_pars = c(1.0, 1.0, 0, 0),
+  laa_pars = c(0.1, 0.1, 0, 0),
   qgain = 0,
   qloss = 0,
   lambda0 = 0,
--- script/job_par2.R
+++ script/job_par2.R
@@ -1,11 +1,11 @@
 library(specmutual)
 load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
-  lac_pars = c(1.0, 1.0),
+  lac_pars = c(0.4, 0.4),
   mu_pars = c(0.2, 0.2, 0.001, 0.001),
   K_pars = c(Inf, Inf, 0.5, 0.5), #mutualism affects immigration with 1.0
   gam_pars = c(0.05, 0.05),
-  laa_pars = c(1.0, 1.0, 0.5, 0.5),
+  laa_pars = c(0.1, 0.1, 0.5, 0.5),
   qgain = 0.01,
   qloss = 0.01,
   lambda0 = 0.5,
