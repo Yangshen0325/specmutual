8a2b0ea8ad18375f3da7d59cef510cbfe9cc890e
Y. Shen <P302656@workspace.rug.nl>
2023-07-06 11:34:06 +0200

    new set

--- script/job_par1.R
+++ script/job_par1.R
@@ -1,16 +1,16 @@
 library(specmutual)
 load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
-  lac_pars = c(0.4, 0.4),
-  mu_pars = c(0.2, 0.2, 0, 0),
-  K_pars = c(Inf, Inf, 0, 0),
-  gam_pars = c(0.05, 0.05),
-  laa_pars = c(0.1, 0.1, 0, 0),
-  qgain = 0,
-  qloss = 0,
-  lambda0 = 0,
+  lac_pars = c(1.0, 1.0),
+  mu_pars = c(0.2, 0.2, 0.001, 0.001),
+  K_pars = c(50, 50, 1.0, 1.0),
+  gam_pars = c(0.04, 0.02),
+  laa_pars = c(0.5, 0.5, 1.0, 1.0),
+  qgain = 0.001,
+  qloss = 0.001,
+  lambda0 = 0.5,
   M0 = M0,
-  transprob = 1
+  transprob = 1.0
 )
 
 set.seed(12)
