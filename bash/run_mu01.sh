7b02265f2fdd0ab1a739e655a9db85cb1b82101c
Yangshen0325 <sylviadyx77@163.com>
2023-09-13 15:18:41 +0200

    modify bash file for job_mu01.

--- bash/run_mu01.sh
+++ bash/run_mu01.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=3-02:00:00
+#SBATCH --time=2-02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=job_par_combo1
-#SBATCH --output=logs/0609-%j.log
-#SBATCH --mem=8GB
+#SBATCH --output=logs/f1209-%j.log
+#SBATCH --mem=16GB
 #SBATCH --partition=regular
 #SBATCH --array=1-11
 
@@ -13,4 +13,4 @@ ml R
 Rscript -e "devtools::install_github('Yangshen0325/specmutual')"
 
 param_set=$SLURM_ARRAY_TASK_ID
-Rscript ~/specmutual/script/job_par_combo1.R ${param_set}
+Rscript ~/specmutual/script/job_mu01.R ${param_set}
