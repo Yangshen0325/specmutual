23f4b2795c55f39281e1c925a2591662fba23564
yang shen <yangshen@RN-145-90-180-46.eduroam.rug.nl>
2023-10-19 15:40:41 +0200

    correct the immigration rates for animal species

--- script/job_set1.R
+++ script/job_set1.R
@@ -4,7 +4,7 @@ mutualism_pars <- create_mutualism_pars(
   lac_pars = c(0.3, 0.3),
   mu_pars = c(0.1, 0.1, 0.001, 0.001),
   K_pars = c(20, 20, 1.0, 1.0),
-  gam_pars = c(0.008, 0.0004),
+  gam_pars = c(0.008, 0.004),
   laa_pars = c(0.5, 0.5, 0.1, 0.1),
   qgain = 0.001,
   qloss = 0.001,
