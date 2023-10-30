94971ffe0b25e046cdd4a8d0957cab9d27d4f7f4
yang shen <yangshen@pc-6-27.customer.ask4.lan>
2023-10-30 21:31:34 +0100

    job set2 and set3

--- script/job_set3.R
+++ script/job_set3.R
@@ -1,14 +1,14 @@
 library(specmutual)
 load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
-  lac_pars = c(0.3, 0.3),
-  mu_pars = c(0.2, 0.2, 0.001, 0.001),
-  K_pars = c(20, 20, 1.0, 1.0),
-  gam_pars = c(0.008, 0.004),
-  laa_pars = c(0.5, 0.5, 0.1, 0.1),
-  qgain = 0.001,
-  qloss = 0.001,
-  lambda0 = 0.05,
+  lac_pars = c(0.6, 0.6),
+  mu_pars = c(0.1, 0.1, 0.01, 0.01),
+  K_pars = c(50, 50, 10, 10),
+  gam_pars = c(0.015, 0.0075),
+  laa_pars = c(0.5, 0.5, 10, 10),
+  qgain = 0.01,
+  qloss = 0.01,
+  lambda0 = 5,
   M0 = M0,
   transprob = 1.0,
   alphaa = 20
