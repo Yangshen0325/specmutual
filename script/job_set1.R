7bd87b7744be558d8d8cfcbacb4a09a296ea20eb
yang shen <yangshen@pc-6-27.customer.ask4.lan>
2023-11-15 17:27:02 +0100

    set5 from 0911 sheet

--- script/job_set1.R
+++ script/job_set1.R
@@ -1,22 +1,30 @@
 library(specmutual)
 load("~/specmutual/script/M0.RData")
 mutualism_pars <- create_mutualism_pars(
-  lac_pars = c(0.3, 0.3),
-  mu_pars = c(0.1, 0.1, 0.001, 0.001),
-  K_pars = c(20, 20, 1.0, 1.0),
-  gam_pars = c(0.008, 0.004),
-  laa_pars = c(0.5, 0.5, 0.1, 0.1),
-  qgain = 0.001,
-  qloss = 0.001,
+  lac_pars = c(0.6, 0.6),
+  mu_pars = c(0.1, 0.1, 0, 0),
+  K_pars = c(50, 50, 1000, 1000),
+  gam_pars = c(0.015, 0.0075),
+  laa_pars = c(0.5, 0.5, 0, 0),
+  qgain = 0.01,
+  qloss = 0.01,
   lambda0 = 0.05,
   M0 = M0,
   transprob = 1.0,
   alphaa = 20
 )
 
-set.seed(1)
-set1 <- peregrine_sim(total_time = 5,
-                      replicates = 100,
-                      mutualism_pars = mutualism_pars,
-                      verbose = TRUE)
-saveRDS(set1, file="~/specmutual/result/set1.rds")
+set.seed(5)
+for (i in 1:100) {
+
+  # Run a replicate simulation
+  set <- specmutual::peregrine_sim(total_time = 5,
+                                   replicates = 1,
+                                   mutualism_pars = mutualism_pars,
+                                   verbose = TRUE)
+
+  # And save it
+  path <- paste0("~/specmutual/result/set5", "_", i, ".rds")
+  saveRDS(set, file = path)
+
+}
