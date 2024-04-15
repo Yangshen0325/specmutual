fa888e32f1898e8406a367101a2d9d860f366d08
yang shen <yangshen@yangs-big-brick.local>
2024-04-15 18:03:27 +0200

    deeted old stuff

--- R/get_gain_rate.R
+++
@@ -1,9 +0,0 @@
-# get gain rates
-get_gain_rate <- function(Mt,
-                          status_p,
-                          t_status_a,
-                          qgain){
-  # both_shown <- (status_p %*% t(status_a)
-  gain_rate <- qgain * ((1 - Mt) * (status_p %*% t_status_a))
-  return(gain_rate)
-}
--- R/plot_web.R
+++
@@ -1,4 +0,0 @@
-# Plot networks
-# We need to use package from "bipartite"
-# plotweb()
-# visweb()
