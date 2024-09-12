b9bfaf40ef42d54c41d892ea79489019f1f03402
yang shen <yangshen@RN-145-90-183-220.eduroam.rug.nl>
2024-09-12 10:14:23 +0200

    .DS_strore

--- R/sim_test_totalrates.R
+++ R/sim_test_totalrates.R
@@ -1,16 +1,23 @@
-# simulation
-# profvis::profvis({sim_mutualism(total_time = 1, mutualism_pars = mutualism_pars)})
-#' Internal function of simulation
-#' @return a named list with island information
+# Simulation for testing rates stuff, also benefit checking parameters. It's
+# the same code as `sim_core_mutualism`, but for saving time and storage, delete
+# something but only print the sum of each kind of rates, the sum of all rates,
+# the sum rate for plant and animal,respectively and the event happening on the island.
+#
+#' @return a named list with island information (not this)
 #'
 sim_test_totalrates <- function(total_time, mutualism_pars) {
+
   #### Initialization ####
+
+  # test if the structure of `mutualism_pars`
   testit::assert(are_mutualism_pars(mutualism_pars))
+
   timeval <- 0
   M0 <- mutualism_pars$M0
   Mt <- M0
+
+  # `alphaa` is alpha in equation, to differentiate with transparency alpha
   alphaa <- mutualism_pars$alphaa
-  M_true_list <- list()
   maxplantID <- nrow(M0)
   maxanimalID <- ncol(M0)
   status_p <- matrix(0, nrow = nrow(M0), ncol = 1)
@@ -18,6 +25,7 @@ sim_test_totalrates <- function(total_time, mutualism_pars) {
 
   island_spec <- c()
   stt_table <- matrix(ncol = 7)
+  # species through time table, `~p` stands for plant species, `~a` animal species
   colnames(stt_table) <- c("Time", "nIp", "nAp", "nCp", "nIa", "nAa", "nCa")
   stt_table[1, ] <- c(total_time, 0, 0, 0, 0, 0, 0)
 
@@ -31,24 +39,22 @@ sim_test_totalrates <- function(total_time, mutualism_pars) {
   lambda0 <- mutualism_pars$lambda0
   transprob <- mutualism_pars$transprob
 
+  # M_true_list <- list()
   # measure_interval <- 0.5
   # measure_time <- measure_interval
 
-
   # if (sum(gam_pars) == 0) {
-  # warning()
-  # return(NULL)# maybe keep it
-  # stop("Island has no species and the rate of
-  #  colonisation is zero. Island cannot be colonised.")
+  #   stop("Island has no species and the rate of
+  #   colonisation is zero. Island cannot be colonised.")
   # }
-  evo_table <- list(c(), NULL)
-  # plant_immi <- list()
-  # animal_immi <- list()
-  # rates_list <- list()
+
+  # evolution table, with the first element represents what event is happenning
+  # ar what time, and sencond element represents
+  # evo_table <- list(c(), NULL)
+
   #### Start Monte Carlo iterations ####
-  # steps <- 0
+
   while (timeval < total_time) {
-    # cat(timeval, dim(Mt), "\n") # for debugging
     rates <- update_rates_mutual(
       M0 = M0,
       Mt = Mt,
@@ -66,38 +72,36 @@ sim_test_totalrates <- function(total_time, mutualism_pars) {
       transprob = transprob,
       island_spec = island_spec
     )
-    print(unlist(lapply(rates, sum)))
-    print(do.call(sum, rates))
-    print(c(sum(status_p), sum(status_a)))
-    # plant_immi[[length(plant_immi) + 1]] <- rates$immig_p
-    # animal_immi[[length(animal_immi) + 1]] <- rates$immig_a
 
-    # rates_list[[length(rates_list) + 1]] <- rates
+    testit::assert(are_rates(rates))
 
-    # testit::assert(are_rates(rates))
+    print(unlist(lapply(rates, sum))) # the the sum for each kind of rates
+    print(do.call(sum, rates)) # the sum for all rates
+    print(c(sum(status_p), sum(status_a))) # the sum rates for all plant n animal
+
     # next time
     timeval_and_dt <- sample_time_mutual(rates = rates, timeval = timeval)
     timeval <- timeval_and_dt$timeval
-    # print(timeval)
-    # steps <- steps + 1
 
+    # save matrix on island every 0.5 time step
     # if (timeval > measure_time &&
     #     timeval - timeval_and_dt$dt < measure_time) {
-    #
     #   M_true <- Mt[which(status_p == 1), which(status_a == 1)]
     #
     #   store_index <- floor(timeval / measure_interval)
     #   M_true_list[[store_index]] <- M_true
-    #
+    #   # print(store_index)
     #   measure_time <- (store_index + 1) * measure_interval
     # }
-    #
+
     if (timeval <= total_time) {
       # next event
       possible_event <- sample_event_mutual(rates = rates)
-      print(possible_event)
-      evo_table[[1]] <- rbind(evo_table[[1]], c(timeval, possible_event))
-      # next state based on event
+      print(possible_event) # the event happening on the island
+
+      # evo_table[[1]] <- rbind(evo_table[[1]], c(timeval, possible_event))
+
+      # next state based on the event
       updated_states <- update_states_mutual(
         M0 = M0,
         Mt = Mt,
@@ -122,63 +126,49 @@ sim_test_totalrates <- function(total_time, mutualism_pars) {
       stt_table <- updated_states$stt_table
     }
   }
-  # cat("Total steps taken:", steps, "\n")
+
   #### Finalize STT ####
-  stt_table <- rbind(
-    stt_table,
-    c(0, stt_table[nrow(stt_table), 2:7])
-  )
-  evo_table[[2]] <- stt_table[nrow(stt_table), ]
+  # stt_table <- rbind(
+  #   stt_table,
+  #   c(0, stt_table[nrow(stt_table), 2:7])
+  # )
+  # evo_table[[2]] <- stt_table[nrow(stt_table), ]
 
   #### Finalize island_spec ####
-  if (length(island_spec) != 0) {
-    cnames <- c(
-      "Species",
-      "Mainland Ancestor",
-      "Colonisation time (BP)",
-      "Species type",
-      "branch_code",
-      "branching time (BP)",
-      "Anagenetic_origin",
-      "Species state"
-    )
-    colnames(island_spec) <- cnames
-    ### set ages as counting backwards from present
-    island_spec[, "branching time (BP)"] <- total_time -
-      as.numeric(island_spec[, "branching time (BP)"])
-    island_spec[, "Colonisation time (BP)"] <- total_time -
-      as.numeric(island_spec[, "Colonisation time (BP)"])
-  }
+  # if (length(island_spec) != 0) {
+  #   cnames <- c(
+  #     "Species",
+  #     "Mainland Ancestor",
+  #     "Colonisation time (BP)",
+  #     "Species type",
+  #     "branch_code",
+  #     "branching time (BP)",
+  #     "Anagenetic_origin",
+  #     "Species state"
+  #   )
+  #   colnames(island_spec) <- cnames
+  #   ### set ages as counting backwards from present
+  #   island_spec[, "branching time (BP)"] <- total_time -
+  #     as.numeric(island_spec[, "branching time (BP)"])
+  #   island_spec[, "Colonisation time (BP)"] <- total_time -
+  #     as.numeric(island_spec[, "Colonisation time (BP)"])
+  # }
+  #
+  # island <- create_island_mutual(
+  #   stt_table = stt_table,
+  #   total_time = total_time,
+  #   island_spec = island_spec
+  # )
+  #
+  #
+  # return(list(
+  #   Mt = Mt,
+  #   M_true_list = M_true_list,
+  #   status_p = status_p,
+  #   status_a = status_a,
+  #   island_spec = island_spec,
+  #   island = island,
+  #   evo_table = evo_table
+  # ))
 
-  island <- create_island_mutual(
-    stt_table = stt_table,
-    total_time = total_time,
-    island_spec = island_spec
-  )
-
-  ## Finalize M_true_list
-  #  for (i in 1:length(M_true_list)) {
-  # check if during the simulation, an Mt was written to a specific entry:
-  #    if (!is.matrix(M_true_list[[i]])) {
-  # if not, we by default place the mainland.
-  # this can happen if at the beginning of the simulation,
-  # a time step is taken that is larger than 'measure_interval', e.g.
-  # with measure_interval 0.5, perhaps the first dt is 1.3, which misses
-  # the measurement at t = 0.5.
-  #      M_true_list[[i]] <- M0
-  #    }
-  #  }
-
-  return(list(
-    Mt = Mt,
-    M_true_list = M_true_list,
-    status_p = status_p,
-    status_a = status_a,
-    island_spec = island_spec,
-    island = island,
-    evo_table = evo_table
-  ))
-  # rates_list = rates_list))
-  # plant_immi = plant_immi,
-  # animal_immi = animal_immi))
 }
