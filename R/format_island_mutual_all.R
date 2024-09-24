1b2a7fb8b23b8fb66d51a4db365560a34d91fa18
yang shen <yangshen@RN-145-90-181-205.eduroam.rug.nl>
2024-09-24 11:54:05 +0200

    rename the function

--- R/format_island_mutual_all.R
+++ R/format_island_mutual_all.R
@@ -1,5 +1,9 @@
-#### format the simulaiton output into standard DAISIE output
-# plant and animal species together
+
+
+#### Format "raw" simulation output into standard DAISIE format, plant and animal are together ####
+
+
+# `island_replicates` is the "raw" simulation output
 
 format_island_mutual_all <- function(island_replicates,
                                      total_time,
@@ -7,15 +11,14 @@ format_island_mutual_all <- function(island_replicates,
                                      mutualism_pars,
                                      verbose) {
   if (is.infinite(sample_freq)) {
-    several_islands <- format_all_full_stt(
+    several_islands <- format_full_stt_all( # without `sample_freq`, full table
       island_replicates = island_replicates,
       total_time = total_time,
-      sample_freq = sample_freq,
       mutualism_pars = mutualism_pars,
       verbose = verbose
     )
   } else {
-    several_islands <- format_all_sampled_stt(
+    several_islands <- format_sampled_stt_all(
       island_replicates = island_replicates,
       total_time = total_time,
       sample_freq = sample_freq,
@@ -25,3 +28,130 @@ format_island_mutual_all <- function(island_replicates,
   }
   return(several_islands)
 }
+
+
+# For a full table, without `sample_freq` ---------------------------------
+
+# Format plant and animal species together without `sample_freq`, so it's a
+# full table containing all information
+
+format_full_stt_all <- function(island_replicates,
+                                total_time,
+                                mutualism_pars,
+                                verbose) {
+  M0 <- mutualism_pars$M0
+  several_islands <- list()
+  for (rep in 1:length(island_replicates)) {
+    the_island <- island_replicates[[rep]][["island"]]
+    the_stt <- the_island$stt_table
+    clades_info_plant <- the_island$clades_info_plant
+    clades_info_animal <- the_island$clades_info_animal
+    clades_info_total <- c(clades_info_plant, clades_info_animal)
+
+    island_list <- list()
+    if (sum(the_stt[nrow(the_stt), 2:7]) == 0) {
+      island_list[[1]] <- list(
+        island_age = total_time,
+        not_present = nrow(M0) + ncol(M0),
+        stt_all = the_stt
+      )
+    } else {
+      island_list[[1]] <- list(
+        island_age = total_time,
+        not_present = nrow(M0) + ncol(M0) - length(clades_info_total),
+        stt_all = the_stt
+      )
+      for (y in 1:length(clades_info_total)) {
+        island_list[[y + 1]] <- clades_info_total[[y]]
+      }
+    }
+    island_list <- DAISIE:::add_brt_table(island_list)
+    several_islands[[rep]] <- island_list
+    if (verbose == TRUE) {
+      message(
+        "Island being formatted: ", rep, "/", length(island_replicates)
+      )
+    }
+  }
+  return(several_islands)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+# For a sampled table, with `sample_freq`  --------------------------------
+
+# Format plant and animal species together with `sample_freq`, so it's ready to plot
+
+format_sampled_stt_all <- function(island_replicates,
+                                   total_time,
+                                   sample_freq,
+                                   mutualism_pars,
+                                   verbose) {
+  M0 <- mutualism_pars$M0
+  several_islands <- list()
+  for (rep in 1:length(island_replicates)) {
+    the_island <- island_replicates[[rep]][["island"]]
+    the_stt <- the_island$stt_table
+    clades_info_plant <- the_island$clades_info_plant
+    clades_info_animal <- the_island$clades_info_animal
+    clades_info_total <- c(clades_info_plant, clades_info_animal)
+
+    stt_all <- matrix(ncol = 7, nrow = sample_freq + 1)
+    colnames(stt_all) <- c("Time", "nIp", "nAp", "nCp", "nIa", "nAa", "nCa")
+    stt_all[, "Time"] <- rev(seq(
+      from = 0,
+      to = total_time,
+      length.out = sample_freq + 1
+    ))
+    stt_all[1, 2:7] <- c(0, 0, 0, 0, 0, 0)
+    for (j in 2:nrow(stt_all)) {
+      the_age <- stt_all[j, "Time"]
+      stt_all[j, 2:7] <- the_stt[max(which(the_stt[, "Time"] >= the_age)), 2:7]
+    }
+
+    island_list <- list()
+    if (sum(the_stt[nrow(the_stt), 2:7]) == 0) {
+      island_list[[1]] <- list(
+        island_age = total_time,
+        not_present = nrow(M0) + ncol(M0),
+        stt_all = stt_all
+      )
+    } else {
+      island_list[[1]] <- list(
+        island_age = total_time,
+        not_present = nrow(M0) + ncol(M0) - length(clades_info_total),
+        stt_all = stt_all
+      )
+      for (y in 1:length(clades_info_total)) {
+        island_list[[y + 1]] <- clades_info_total[[y]]
+      }
+    }
+
+    island_list <- DAISIE:::add_brt_table(
+      island = island_list,
+      full_table = FALSE
+    )
+    several_islands[[rep]] <- island_list
+    if (verbose == TRUE) {
+      message(
+        "Island being formatted: ", rep, "/", length(island_replicates)
+      )
+    }
+  }
+  return(several_islands = several_islands)
+}
+
+
+
+
+
+
