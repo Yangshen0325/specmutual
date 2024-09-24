33106f796a790fe96b13f7e4e4cb59699ab52872
yang shen <yangshen@yangs-big-brick.local>
2024-09-24 21:36:45 +0200

    add more info for the readability of the code

--- R/sample_event_time.R
+++ R/sample_event_time.R
@@ -1,4 +1,21 @@
-# sample the possible event
+
+#' Samples what event to happen next
+#'
+#' @return Numeric indicating what event will happen
+#' \itemize{
+#'   \item{[1]: immigration event with plant species}
+#'   \item{[2]: extinction event with plant species}
+#'   \item{[3]: cladogenesis event with plant species}
+#'   \item{[4]: anagenesis event with plant species}
+#'   \item{[5]: immigration event with animal species}
+#'   \item{[6]: extinction event with animal species}
+#'   \item{[7]: cladogenesis event with animal species}
+#'   \item{[8]: anagenesis event with animal species}
+#'   \item{[9]: cospeciation event between pairs}
+#'   \item{[10]: gain links event between pairs}
+#'   \item{[11]: loss links event between pairs}
+#' }
+
 sample_event_mutual <- function(rates) {
   testit::assert(are_rates(rates))
 
@@ -25,7 +42,17 @@ sample_event_mutual <- function(rates) {
   testit::assert(possible_event >= 1)
   return(possible_event)
 }
-# sample next time value
+
+
+#' Samples the next time value
+#'
+#' @param timeval Current time of simulation
+#' @param rates A named list of all kinds of rates as returned by
+#' \code{\link{update_rates_mutual}}.
+#'
+#' @return A named list with numeric vector containing the time of the next
+#' timestep and the change in time.
+#'
 sample_time_mutual <- function(rates, timeval) {
   totalrate <- do.call(sum, rates)
 
