94195aa57efe34c380d963e0d019c03864cb3be4
yang shen <yangshen@RN-145-90-181-205.eduroam.rug.nl>
2024-09-25 16:17:58 +0200

    Improve code and add more description

--- R/sim_mutualism.R
+++ R/sim_mutualism.R
@@ -1,5 +1,22 @@
 #' @title sim_mutualism
+#' @description Simulates mutualistic interactions over macro-evolutionary time with specified parameters.
+#'
+#' @param total_time Numeric. Total time for the simulation.
+#' @param replicates Integer. Number of replicates to run.
+#' @param mutualism_pars List. Parameters used in the simulation.
+#' @param sample_freq Numeric. Frequency of sampling data, fpr plotting.
+#' @param cond_p Integer. Condition for plants (e.g., minimum number of present plants on the island).
+#' @param cond_a Integer. Condition for animals (e.g., minimum number of present animals on the island).
+#' @param verbose Logical. If TRUE, prints progress updates.
+#'
+#' @return A list containing:
+#'   - island_replicates: List of island replicates from simulations.
+#'   - island_segments: Formatted data for plants and animals separated.
+#'   - island_total: Formatted data for plants and animals combined.
+#'
 #' @export sim_mutualism
+#'
+#'
 sim_mutualism <- function(total_time,
                           replicates,
                           mutualism_pars,
@@ -7,35 +24,37 @@ sim_mutualism <- function(total_time,
                           cond_p,
                           cond_a,
                           verbose) {
-  island_replicates <- list()
-  for (rep in 1:replicates) {
-    if (cond_p == 0 & cond_a == 0) {
-      number_present_p <- -1
-      number_present_a <- -1
-    } else {
-      number_present_p <- 0
-      number_present_a <- 0
-    }
+  # Initialize list to store island replicates
+  island_replicates <- vector("list", replicates)
+
+  for (rep in seq_len(replicates)) {
+    # Check conditions for presence of plants and animals
+    number_present_p <- ifelse(cond_p == 0 & cond_a == 0, -1, 0)
+    number_present_a <- ifelse(cond_p == 0 & cond_a == 0, -1, 0)
+
+    # Simulation loop until conditions are met
     while (number_present_p < cond_p | number_present_a < cond_a) {
+
       island_replicates[[rep]] <- sim_core_mutualism(
         total_time = total_time,
         mutualism_pars = mutualism_pars
       )
+
+      # Extract the "island" information. This island has the same structure as it is in DAISIE
       temp_island <- island_replicates[[rep]][["island"]]
-      clades_plant <- temp_island[["clades_info_plant"]]
-      clades_animal <- temp_island[["clades_info_animal"]]
-      stac_vec_p <- unlist(clades_plant)[which(names(unlist(clades_plant)) == "stac")]
-      stac_vec_a <- unlist(clades_animal)[which(names(unlist(clades_animal)) == "stac")]
-      present_p <- which(stac_vec_p != 0)
-      present_a <- which(stac_vec_a != 0)
-      number_present_p <- length(present_p)
-      number_present_a <- length(present_a)
+
+      # Calculate present clades on the island
+      number_present_p <- length(temp_island[["clades_info_plant"]])
+      number_present_a <- length(temp_island[["clades_info_animal"]])
     }
-    if (verbose == TRUE) {
-      print(paste("Island replicate ", rep, sep = ""))
+
+    # Verbose output for progress tracking
+    if (verbose) {
+      message(paste("Island replicate", rep))
     }
   }
-  # Format data, plant and animal are separated
+
+  # Format data for plants and animals separately
   island_segments <- format_island_mutual_pa(
     island_replicates = island_replicates,
     total_time = total_time,
@@ -44,7 +63,7 @@ sim_mutualism <- function(total_time,
     verbose = verbose
   )
 
-  # Format data, plant and animal are together
+  # Format combined data for plants and animals
   island_total <- format_island_mutual_all(
     island_replicates = island_replicates,
     total_time = total_time,
