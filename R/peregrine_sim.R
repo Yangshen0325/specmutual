438075cfe4a3012c1df9c2e2d76ab5b487100708
Yangshen0325 <sylviadyx77@163.com>
2024-10-29 16:24:42 +0100

    specify the returns

--- R/peregrine_sim.R
+++ R/peregrine_sim.R
@@ -11,12 +11,14 @@
 peregrine_sim <- function(total_time,
                           replicates,
                           mutualism_pars,
+                          return_parts,
                           verbose = TRUE) {
   island_replicates <- list()
   for (rep in 1:replicates) {
     island_replicates[[rep]] <- sim_core_mutualism(
       total_time = total_time,
-      mutualism_pars = mutualism_pars
+      mutualism_pars = mutualism_pars,
+      return_parts = return_parts
     )
     if (verbose == TRUE) {
       print(paste("Island replicate ", rep, sep = ""))
