6d8316a42737f6b8dbfb3dbb5bb33b4badd633f1
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-02-18 15:16:08 +0100

    Network that are smaller than 2*2 is not meaningful

--- R/peregrine_sim.R
+++ R/peregrine_sim.R
@@ -14,16 +14,37 @@ peregrine_sim <- function(total_time,
                           return_parts,
                           verbose = TRUE) {
   island_replicates <- list()
-  for (rep in 1:replicates) {
-    island_replicates[[rep]] <- sim_core_mutualism(
+
+  tried <- 0
+  i <- 1
+
+  while(i <= replicates) {
+
+    sim_output <- sim_core_mutualism(
       total_time = total_time,
       mutualism_pars = mutualism_pars,
       return_parts = return_parts
     )
-    if (verbose == TRUE) {
-      print(paste("Island replicate ", rep, sep = ""))
+
+    Mt <- sim_output$Mt
+    status_p <- sim_output$status_p
+    status_a <- sim_output$status_a
+    true_Mt <- Mt[status_p == 1, status_a == 1, drop = FALSE]
+
+    tried <- tried + 1
+
+    # Check if the network is at least 2 x 2
+    if(nrow(true_Mt) >= 2 && ncol(true_Mt) >= 2) {
+      island_replicates[[i]] <- sim_output
+      i <- i + 1
+      if (verbose == TRUE) {
+        print(paste("Island replicate ", i, sep = ""))
+      }
+    } else {
+      message("Simulation attempt ", tried, " dropped: true_Mt is smaller than 2x2.")
     }
   }
 
   return(island_replicates = island_replicates)
+
 }
